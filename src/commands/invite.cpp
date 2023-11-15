486d65ecbe757e2231a36a0f43fe444221a0eed0
gclement42 <clement.games125@gmail.com>
2023-11-15 10:25:46 +0100

    add invite.cpp

---
+++ src/commands/invite.cpp
@@ -0,0 +1,27 @@
+# include "main.hpp"
+
+void Commands::invite() {
+	if (_args.size() != 3) {
+		_client.addMessageToSend(ERR_NEEDMOREPARAMS(_args[0]));
+		return ;
+	}
+	std::string channelName = _args[2];
+	std::string targetNickname = _args[1];
+	if (!this->checkIfChannelExist(channelName)) {
+		_client.addMessageToSend(ERR_NOSUCHCHANNEL(_client.getNickname(), channelName));
+		return ;
+	}
+	if (!this->checkIfThisUserIsOnChannel(channelName)) {
+		_client.addMessageToSend(ERR_NOTONCHANNEL(_client.getNickname(), channelName));
+		return ;
+	}
+	if (this->checkIfTargetClientIsOnChannel(channelName, targetNickname)) {
+		_client.addMessageToSend(ERR_USERONCHANNEL(_client.getNickname(), channelName));
+		return ;
+	}
+	Client &target = this->getClientFromNickname(targetNickname);
+	target.addMessageToSend(RPL_INVITE(_client.getNickname(), targetNickname, channelName));
+	target.setWaitingForSend(true);
+	_client.addMessageToSend(RPL_INVITING(_client.getNickname(), targetNickname, channelName));
+}
+
