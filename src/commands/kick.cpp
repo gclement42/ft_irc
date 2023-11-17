2a7712765d312b352ab394627e1484a26c4742d7
maujogue <maujogue@ik.me>
2023-11-17 11:17:39 +0100

    parseComaSeparatedArgs works

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -18,35 +18,43 @@ std::vector<std::string>	parseComaSeparatedArgs(std::vector<std::string> arg)
 {
 	std::vector<std::string> 	result;
 	std::string::iterator it;
-	std::stringstream ss;
 	std::string token;
-	std::cout << "DEBUG :"<< std::endl;
-	for (size_t i = 0; i < arg.size(); i++) {
-		ss.str(arg[i]);
-		std::cout << arg[i] << std::endl;
-		while (std::getline(ss, token, ',')) {
-			result.push_back(token);
+	size_t i = 1;
+
+	while (arg[i].find("#") != std::string::npos)
+		i++;
+	if (arg[i].find(",") != std::string::npos)
+	{
+		std::stringstream ss(arg[i]);
+		while (std::getline(ss, token, ','))
+		{
+			if (token[0] == ':')
+				token.erase(0, 1);
+			result.push_back(token.erase(0,1));
 		}
 	}
-	std::cout << "END DEBUG :"<< std::endl;
+	else
+	{
+		if (arg[i][0] == ':')
+			arg[i].erase(0, 1);
+		result.push_back(arg[i]);
+	}
 	return (result);
 }
 
 void	Commands::kick()
 {
 	std::vector<std::string> channelNameTab = this->parseChannelName(this->_args);
-	for (size_t i = 0; i < channelNameTab.size(); i++)
-		std::cout << channelNameTab[i] << std::endl;
+//	for (size_t i = 0; i < channelNameTab.size(); i++)
+//		std::cout << channelNameTab[i] << std::endl;
 	if (channelNameTab.size() > 2)
 		return ;
 	else if (channelNameTab.empty())
 		this->_client.addMessageToSend(ERR_NEEDMOREPARAMS(this->_client.getNickname(), "KICK"));
 
 	std::vector<std::string> usersTab = parseComaSeparatedArgs(this->_args);
-	std::cout << "OK" << std::endl;
 	for (size_t i = 0; i < usersTab.size(); i++)
 		std::cout << usersTab[i] << std::endl;
-	std::cout << "OK" << std::endl;
 	std::map<std::string, Channel>::iterator channel = this->_channels.find(channelNameTab[0]);
 	std::cout << channel->first << std::endl;
 }
