b44e80d019b7ca0afd69e4f2adecd41bb1660ad3
boulatrousse <lboulatrous@gmail.com>
2023-11-21 14:16:08 +0100

    Bot Alfred with responses done

--- BotAlfred/header/Bot.hpp
+++ BotAlfred/header/Bot.hpp
@@ -6,9 +6,69 @@
 /*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:05:18 by lboulatr          #+#    #+#             */
-/*   Updated: 2023/11/20 13:06:11 by lboulatr         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:01:05 by lboulatr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BOT_HPP
 # define BOT_HPP
+
+# include <iostream>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <poll.h>
+# include <unistd.h>
+# include <string.h>
+# include <ctime>
+# include <sstream>
+# include <sstream>
+# include <vector>
+# include <fcntl.h>
+# include <cerrno>
+# include <cstring>
+# include <csignal>
+# include <cstdlib>
+# include <algorithm>
+# include <map>
+
+# define SUCCESS 0
+# define FAILURE 1
+
+class Bot
+{
+	public:
+
+		Bot(int port, std::string password, std::string ip);
+		~Bot(void);
+		
+		int 					startBot(void);
+		void 					closeBot(void);
+		void 					runBot(void);
+		std::string 			readInBuffer(int socket);
+		void 					botResponse(std::string buffer, int socket);
+		
+		std::map<std::string, std::string>		createResponsesMap(void);
+		void 									newUserInChannel(int socket);
+		void 									privateMsgResponse(std::string buffer, int socket);
+
+	private:
+
+		int 					_socket;
+		int 					_port;
+		
+		std::string 			_serverPassword;
+		std::string 			_ip;
+		std::string 			_botPassword;
+		std::string 			_nickName;
+		std::string 			_userName;
+		
+		struct sockaddr_in 		_addr;
+
+		std::map<std::string, std::string>		_responses;
+	
+};
+
+void 	printError(std::string error);
+
+#endif
--- BotAlfred/src/main.cpp
+++ BotAlfred/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:03:16 by lboulatr          #+#    #+#             */
-/*   Updated: 2023/11/20 13:04:14 by lboulatr         ###   ########.fr       */
+/*   Updated: 2023/11/21 09:06:38 by lboulatr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,22 @@ int main(int argc, char **argv)
 	int 			port;
 	std::string 	password;
 
+	if (argc != 3)
+		return (printError("Usage: ./bot [port] [password]"), FAILURE);
+
 	port = atoi(argv[1]);
 	password = argv[2];
-	
-	return (0);
+
+	Bot bot(port, password, "ptdr");
+	bot.startBot();
+	bot.runBot();
+	bot.closeBot();
+	return (SUCCESS);
+}
+
+void printError(std::string error)
+{
+	std::cout << "\033[1;31m";
+	std::cout << error << std::endl;
+	std::cout << "\033[0m";
 }
